62cea2c3e7be10e5ab90cf5f33f1e9039c7e705c
Rinam <rannamaharoof@gmail.com>
2024-12-08 00:12:30 +0530

    nav bar color change

--- frontend/src/Components/HomePage/NavBar.js
+++ frontend/src/Components/HomePage/NavBar.js
@@ -3,7 +3,7 @@ import './NavBarStyles.css';  // Import the CSS file
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <a className="navbar-brand" href="#">
           <img 
