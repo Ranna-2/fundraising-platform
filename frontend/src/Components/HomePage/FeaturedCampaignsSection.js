dfdf6eb60a2f4518737f3b588dff718dfa9ee0a9
Rinam <rannamaharoof@gmail.com>
2024-12-21 17:29:39 +0530

    image additions & sorting

--- frontend/src/Components/HomePage/FeaturedCampaignsSection.js
+++ frontend/src/Components/HomePage/FeaturedCampaignsSection.js
@@ -7,7 +7,7 @@ const FeaturedCampaignsSection = () => {
             <h2>Featured Campaigns</h2>
             <div className="card-container">
                 <div className="card">
-                    <img src="/images/disaster2.jpg" alt="Food & Water Relief" />
+                    <img src="/images/featuredCampaigns/disaster2.jpg" alt="Food & Water Relief" />
                     <div className="card-content">
                         <h3 className="card-title">Food & Water Relief</h3>
                         <p className="card-description">Delivering essential food supplies and clean drinking water to affected regions after a disaster.</p>
@@ -15,7 +15,7 @@ const FeaturedCampaignsSection = () => {
                     </div>
                 </div>
                 <div className="card">
-                    <img src="/images/program1.jpg" alt="Free Health Checkups" />
+                    <img src="/images/featuredCampaigns/program1.jpg" alt="Free Health Checkups" />
                     <div className="card-content">
                         <h3 className="card-title">Free Health Checkups</h3>
                         <p className="card-description">Providing free health checkup camps for underprivileged communities to detect and treat diseases early.</p>
@@ -23,7 +23,7 @@ const FeaturedCampaignsSection = () => {
                     </div>
                 </div>
                 <div className="card">
-                    <img src="/images/hunger5.jpg" alt="School Meal Programs" />
+                    <img src="/images/featuredCampaigns/hunger5.jpg" alt="School Meal Programs" />
                     <div className="card-content">
                         <h3 className="card-title">School Meal Programs</h3>
                         <p className="card-description">Ensuring that children in need receive daily meals to keep them healthy and focused in school.</p>
